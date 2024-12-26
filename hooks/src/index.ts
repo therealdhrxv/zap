12710e6bf9ac8ae9eaf78f5b97224e6820be79dc
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-12-26 17:31:14 +0530

    outbox pattern implementation

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,39 @@
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+
 // eg: https://hooks.zapier.com/hooks/catch/17038168/285ebaj/
 
 const app = express();
+app.use(express.json());
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+const client = new PrismaClient();
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
 
 	const { userId, zapId } = req.params;
 
-	// store it in a db
-	// push it on a queue (kafka / redis)
-	
+	const body = req.body;
+
+	// outbox pattern for microservices architectures
+	await client.$transaction(async tx => {
+		const run = await tx.zapRun.create({
+			data: {
+				zapId: zapId,
+				metadata: body,
+			}
+		})
+		await tx.zapRunOutbox.create({
+			data: {
+				zapRunId: run.id,
+			}
+		})
+	})
+ 
+	res.json({
+		message: "Webhook successfully hit",
+	})
+
 });
+
+app.listen(3000);
