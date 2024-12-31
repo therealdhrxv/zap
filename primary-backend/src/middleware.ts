2f5781d629aa1f733bf115f37c01d4e0c448d2e3
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2024-12-31 12:44:23 +0530

    middleware

--- primary-backend/src/middleware.ts
+++ primary-backend/src/middleware.ts
@@ -1,5 +1,22 @@
+import jwt from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+import { JWT_PASSOWRD } from "./config";
 
+export function authMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	const token = req.headers.authorization as unknown as string;
+	try {
+		const payload = jwt.verify(token, JWT_PASSOWRD);
+		// @ts-ignore
+		req.id = payload.id;
+		next();
+	} catch (error) {
+		res.status(401).json({
+			message: "You are not logged in",
+		});
+	}
 }
